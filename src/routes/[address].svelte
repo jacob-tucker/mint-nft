d79b0fe97fd8fc702a2140135688d63965854c7d
Jacob Tucker <tsnakejake@aol.com>
2022-02-19 14:46:29 -0600

    update styling on account page

--- src/routes/[address].svelte
+++ src/routes/[address].svelte
@@ -6,13 +6,14 @@
   let nfts = getNFTs($page.params.address)
 </script>
 
-<div>
+<div class="all">
   <h1>All your NFTs</h1>
 
   {#await nfts}
     <h1>Loading...</h1>
   {:then nfts}
     {#if nfts?.length > 0}
+    <div class="list">
       {#each nfts as nft}
         <article class="card-preview">
           <img src={`https://ipfs.infura.io/ipfs/${nft.image}`} alt="nft" />
@@ -20,6 +21,7 @@
           <small>Created on {new Date(nft.date * 1000).toLocaleString()}</small>
         </article>
       {/each}
+    </div>
     {:else}
       <p>This account doesn't have any FLOATs yet.</p>
     {/if}
@@ -27,10 +29,15 @@
 </div>
 
 <style>
-  div {
+  .all {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
   }
 </style>
