6d4fd61c463939b52e17e29f3dd057b1b4df318c
Jacob Tucker <tsnakejake@aol.com>
2022-02-19 14:38:55 -0600

    update config

--- src/lib/flow/config.js
+++ src/lib/flow/config.js
@@ -1,8 +1,8 @@
 import { config } from "@samatech/onflow-fcl-esm";
 
 config({
-  "accessNode.api": import.meta.env.VITE_ACCESS_NODE_API,
-  "discovery.wallet": import.meta.env.VITE_DISCOVERY_WALLET,
+  "accessNode.api": "https://access-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
   "0xExampleNFT": "0x3f55c5f48f39b076",
   "0xMDV": "0x631e88ae7f1d7c20",
   "0xNFT": "0x631e88ae7f1d7c20"
